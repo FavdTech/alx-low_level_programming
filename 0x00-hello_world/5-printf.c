59d463f4a4b8a842aace5891ed7d7da287d5b5c4
Angel <favouredangela@gmail.com>
2022-09-10 17:41:53 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Description: prints "with proper grammar, but the outcome is a piece of art,
+*main - Entry point
+*Description: prints "with proper grammar, but the outcome is a piece of art,
 */
 int main(void)
 {
